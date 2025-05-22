17bf7edd01551bdd0dc63e1d9e83f7b14eaf800e
Sâmpetru D. Mario <119307188+mariosam23@users.noreply.github.com>
2025-05-22 20:21:49 +0300

    Pushed

--- demo/src/main/java/app/demo/controllers/ReviewController.java
+++ demo/src/main/java/app/demo/controllers/ReviewController.java
@@ -12,7 +12,6 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import java.nio.file.AccessDeniedException;
 
 @RestController
 @RequestMapping("/api/reviews")
